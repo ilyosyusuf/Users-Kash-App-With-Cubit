ccb75fa75dca265c0e8b52162f1a30c29811a846
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-07-31 13:32:51 +0500

    base view service was added

--- lib/screens/view/home_view.dart
+++ lib/screens/view/home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:users_cubit/core/base/base_view.dart';
 import 'package:users_cubit/core/boxes/boxes.dart';
 import 'package:users_cubit/core/constants/color_const.dart';
 import 'package:users_cubit/model/user_model.dart';
@@ -28,70 +29,76 @@ class HomeView extends StatelessWidget {
 
   Scaffold scaffoldMethod(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: ColorConst.kSecondaryColor,
-          actions: const [
-            Padding(
-              padding: EdgeInsets.all(8.0),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/kashapp.png'),
-              ),
-            )
-          ],
-          title: const Text("Users"),
-        ),
-        body: SafeArea(
-          child: BlocBuilder<HomeCubit, HomeState>(
-            builder: (context, state) {
-              if (state is HomeLoadingState) {
-                return ListView.builder(
-                  itemCount: 15,
-                  itemBuilder: (context, i) {
-                    return ShimmerListTileWidget();
-                  },
-                );
-              }
-              if (state is HomeErrorState) {
-                return Center(child: Text(state.message));
-              } else if (state is HomeLoadedState) {
-                return RefreshIndicator(
-                  onRefresh: () async {
-                    context.read<HomeCubit>().refreshData();
-                  },
-                  child: ValueListenableBuilder<Box<UserModel>>(
-                    valueListenable: Boxes.instance.getUserBox().listenable(),
-                    builder: (context, box, i) {
-                      final users = box.values.toList().cast<UserModel>();
-                      return ListView.builder(
-                        itemCount: users.length,
-                        itemBuilder: (context, i) {
-                          return Dismissible(
-                            direction: DismissDirection.endToStart,
-                            key: UniqueKey(),
-                            background: Container(
-                                margin: const EdgeInsets.all(20),
-                                color: ColorConst.kRedColor),
-                            onDismissed: (v) {
-                              HiveService.instance.deleteData(users[i]);
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: ColorConst.kSecondaryColor,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage('assets/images/kashapp.png'),
+            ),
+          )
+        ],
+        title: const Text("Users"),
+      ),
+      body: BaseView(
+          viewModel: HomeView,
+          OnPageBuilder: (context, widget) {
+            return SafeArea(
+              child: BlocBuilder<HomeCubit, HomeState>(
+                builder: (context, state) {
+                  if (state is HomeLoadingState) {
+                    return ListView.builder(
+                      itemCount: 15,
+                      itemBuilder: (context, i) {
+                        return const ShimmerListTileWidget();
+                      },
+                    );
+                  }
+                  if (state is HomeErrorState) {
+                    return Center(child: Text(state.message));
+                  } else if (state is HomeLoadedState) {
+                    return RefreshIndicator(
+                      onRefresh: () async {
+                        context.read<HomeCubit>().refreshData();
+                      },
+                      child: ValueListenableBuilder<Box<UserModel>>(
+                        valueListenable:
+                            Boxes.instance.getUserBox().listenable(),
+                        builder: (context, box, i) {
+                          final users = box.values.toList().cast<UserModel>();
+                          return ListView.builder(
+                            itemCount: users.length,
+                            itemBuilder: (context, i) {
+                              return Dismissible(
+                                direction: DismissDirection.endToStart,
+                                key: UniqueKey(),
+                                background: Container(
+                                    margin: const EdgeInsets.all(20),
+                                    color: ColorConst.kRedColor),
+                                onDismissed: (v) {
+                                  HiveService.instance.deleteData(users[i]);
+                                },
+                                child: ListTileWidget(
+                                    itemColor: ColorConst.kSecondaryColor,
+                                    leadingColor: ColorConst.kPrimaryColor,
+                                    userId: users[i].id.toString(),
+                                    userName: users[i].name,
+                                    userEmail: users[i].email),
+                              );
                             },
-                            child: ListTileWidget(
-                                itemColor: ColorConst.kSecondaryColor,
-                                leadingColor: ColorConst.kPrimaryColor,
-                                userId: users[i].id.toString(),
-                                userName: users[i].name,
-                                userEmail: users[i].email),
                           );
                         },
-                      );
-                    },
-                  ),
-                );
-              } else {
-                return throw Exception("Error with states");
-              }
-            },
-          ),
-        ));
+                      ),
+                    );
+                  } else {
+                    return throw Exception("Error with states");
+                  }
+                },
+              ),
+            );
+          }),
+    );
   }
 }
