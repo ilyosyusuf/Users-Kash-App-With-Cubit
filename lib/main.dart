b4297e6892f0c4c373a5359692b3c3dbe68ec022
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-07-31 12:43:06 +0500

    home cubit was added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:users_cubit/repositories/user_repository.dart';
+import 'package:users_cubit/screens/view/home_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +17,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: MultiRepositoryProvider(providers: [
+          RepositoryProvider(create: (context) => UserRepository())
+        ], child: HomeView()),
     );
   }
 }
---
+++ lib/screens/cubit/home_cubit.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:users_cubit/repositories/user_repository.dart';
+import 'package:users_cubit/screens/state/home_state.dart';
+
+
+class HomeCubit extends Cubit<HomeState>{
+  final UserRepository userRepository;
+
+  HomeCubit(this.userRepository) : super(HomeLoadingState()) {
+    loadData();
+  }
+
+
+
+  Future loadData() async {
+        try {
+      emit(HomeLoadingState());
+      final users = await userRepository.getUsers();
+      print("initial");
+      emit(HomeLoadedState(users!));
+    } catch (e) {
+      emit(HomeErrorState(message: "Error at loading user data"));
+    }
+  }
+
+  Future refreshData() async {
+        try {
+      emit(HomeLoadingState());
+      final users = await userRepository.getUsers();
+      print("initial");
+      emit(HomeLoadedState(users!));
+    } catch (e) {
+      emit(HomeErrorState(message: "Error at refreshing user data"));
+    }
+  }
+
+}
